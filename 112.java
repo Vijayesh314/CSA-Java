25af7a4f683ecae3028b021b6dd2898c0fa0b53e
Vijayesh314 <145930677+Vijayesh314@users.noreply.github.com>
2025-08-20 11:51:41 -0700

    Update 112.java

--- 112.java
+++ 112.java
@@ -1,18 +1,51 @@
-public class FirstVariable
+public class Allowance
 {
- 	public static void main(String[] args)
- 	{
-    int age = 12;
-    double allowance = 5.25;
-    boolean isOldEnough = true;
- 
-    System.out.print("The value of age is ");
-    System.out.println(age);
-    float age;
-    System.out.prinln(age);
-    System.out.print("The value of allowance is ");
-    System.out.println(allowance);
-    System.out.print("The value of isOldEnough is ");
-    System.out.println(isOldEnough );
- 	}
+	public static void main(String[] args)
+	{
+		final float ALLOWANCE = 8.75;
+  int age = 16;
+		boolean isOldEnough = true;
+  System.out.print("Gina’s age is ");
+		System.out.println(age);
+		System.out.print("Eligible for allowance? ");
+		System.out.println(isOldEnough);
+		System.out.print("Gina’s weekly allowance is ");
+		System.out.println(ALLOWANCE);
+
+		// Carl's information
+		int age = 16 - 3;
+		if (age >= 8) {
+         isOldEnough = true;
+      } else {
+         isOldEnough = false;
+      }
+		System.out.print("Carl’s age is ");
+		System.out.println(age);
+		System.out.print("Eligible for allowance? ");
+		System.out.println(isOldEnough);
+		System.out.print("Carl’s weekly allowance is ");
+		if (isOldEnough) {
+     System.out.println(ALLOWANCE);
+  } else {
+     System.out.println(0);
+  }
+
+		// Jennifer's information
+		int age = (16 - 3) - 10;
+  if (age >= 8) {
+         isOldEnough = true;
+      } else {
+         isOldEnough = false;
+      }
+		System.out.print("Jennifer’s age is ");
+		System.out.println(age);
+		System.out.print("Eligible for allowance? ");
+		System.out.println(isOldEnough);
+		System.out.print("Jennifer’s weekly allowance is ");
+		if (isOldEnough) {
+         System.out.println(ALLOWANCE);
+      } else {
+         System.out.println(0);
+      }
+	}
 }

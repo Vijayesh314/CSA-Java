76d19bc97e8cae0ee5e413cedbe4c51169fe68ac
Vijayesh314 <145930677+Vijayesh314@users.noreply.github.com>
2025-08-20 11:58:27 -0700

    Update 112.java

--- 112.java
+++ 112.java
@@ -3,9 +3,9 @@ public class Allowance
 	public static void main(String[] args)
 	{
 		final float ALLOWANCE = 8.75;
-  int age = 16;
+  		int age = 16;
 		boolean isOldEnough = true;
-  System.out.print("Gina’s age is ");
+  		System.out.print("Gina’s age is ");
 		System.out.println(age);
 		System.out.print("Eligible for allowance? ");
 		System.out.println(isOldEnough);
@@ -16,27 +16,27 @@ public class Allowance
 		int age = 16 - 3;
 		if (age >= 8) {
          isOldEnough = true;
-      } else {
+      	} else {
          isOldEnough = false;
-      }
+      	}
 		System.out.print("Carl’s age is ");
 		System.out.println(age);
 		System.out.print("Eligible for allowance? ");
 		System.out.println(isOldEnough);
 		System.out.print("Carl’s weekly allowance is ");
 		if (isOldEnough) {
-     System.out.println(ALLOWANCE);
-  } else {
-     System.out.println(0);
-  }
+     	System.out.println(ALLOWANCE);
+  		} else {
+     	System.out.println(0);
+  		}
 
 		// Jennifer's information
 		int age = (16 - 3) - 10;
-  if (age >= 8) {
+  		if (age >= 8) {
          isOldEnough = true;
-      } else {
+      	} else {
          isOldEnough = false;
-      }
+      	}
 		System.out.print("Jennifer’s age is ");
 		System.out.println(age);
 		System.out.print("Eligible for allowance? ");
@@ -44,7 +44,7 @@ public class Allowance
 		System.out.print("Jennifer’s weekly allowance is ");
 		if (isOldEnough) {
          System.out.println(ALLOWANCE);
-      } else {
+      	} else {
          System.out.println(0);
       }
 	}

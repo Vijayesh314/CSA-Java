75c57f4ddd1c7613136d095cab5482ba8a565895
Vijayesh314 <145930677+Vijayesh314@users.noreply.github.com>
2025-10-24 11:25:41 -0700

    Update DeMorgansLawGTLT.java

--- 136/DeMorgansLawGTLT.java
+++ 136/DeMorgansLawGTLT.java
@@ -11,27 +11,19 @@ public class DeMorgansLawGTLT
 
     System.out.println("Enter an integer for Y:");
     int y = sc.nextInt(); 
-    
-    System.out.println("Checking that both x and y are not negative (Original &&): ");
-    if (!(x < 0) && !(y < 0)) {
-      System.out.println("Result: Both x and y are not negative.");
-    } else {
-      System.out.println("Result: At least one of the numbers is negative.");
-    }
 
     System.out.println("\nChecking that x < 0 AND y < 0 is FALSE (New OR): ");
     if (!(x < 0 || y < 0)) {
       System.out.println("Result: The condition (x < 0 OR y < 0) is FALSE.");
-      System.out.println("This confirms that both x and y are not negative.");
+      System.out.println("This confirms that both integers x and y are not negative.");
     } else {
       System.out.println("Result: The condition (x < 0 OR y < 0) is TRUE.");
-      System.out.println("This confirms that at least one of the numbers is negative.");
+      System.out.println("This confirms that either integer x or y is negative.");
     }
 
     /// Short-circuit evaluation could occur at the || operator in: !(x < 0 || y < 0)
     /// If the first condition, (x < 0), evaluates to TRUE (i.e., x is negative), the entire OR expression (x < 0 || y < 0) is instantly TRUE. 
     /// The second condition, (y < 0), will then not be evaluated (it is short-circuited).
-    
 
     sc.close();
   }
